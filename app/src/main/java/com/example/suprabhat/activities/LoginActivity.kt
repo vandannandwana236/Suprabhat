be8a88821d164963dfdda020ab1f8038026c9ef8
Vandan Nandwana <68931808+vaibs011101101@users.noreply.github.com>
2023-09-13 18:31:49 +0530

    Initial Commit

--- app/src/main/java/com/example/suprabhat/activities/LoginActivity.kt
+++ app/src/main/java/com/example/suprabhat/activities/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.suprabhat.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -10,5 +11,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val sigup_text = findViewById<TextView>(R.id.signup_textview)
+        sigup_text.setOnClickListener{
+            intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
